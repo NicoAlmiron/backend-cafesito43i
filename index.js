590d61a3e5797a89724a71654ffa92e086c91aae
Nico Almiron <almiron.nicolas16@gmail.com>
2023-08-23 21:06:11 -0300

    agregamos la configuracion de los modulos

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('esto es una prueba')
+
+// 1- configuraciones iniciales
+import express from 'express';
+
+// 2- middlewares 
+
+// 3- crear las rutas (siempre despues de los middlewares)
