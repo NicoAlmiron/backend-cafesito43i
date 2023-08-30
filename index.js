7a0a5db9e64775696ee0ad1af43c34b4f6e6dae7
Nico Almiron <almiron.nicolas16@gmail.com>
2023-08-30 20:11:20 -0300

    agregamos un puerto al backend

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ console.log('esto es una pruebita')
 
 const app = express();
 //crear una variable con express
-app.set('port', process.env.PORT || 4004)
+app.set('port', process.env.PORT || 4000)
+app.listen(app.get('port'), () => {
+    console.log('estoy escuchando en el puerto: ' + app.get('port'))
+})
 
 // 2- middlewares 
 
