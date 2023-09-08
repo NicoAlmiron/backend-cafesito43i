f8beb5e0f5ba887a1ed9398e54333ab823669de9
Nico Almiron <almiron.nicolas16@gmail.com>
2023-09-08 15:21:59 -0300

    se corrieron errores encontrados

--- src/database/database.js
+++ src/database/database.js
@@ -5,5 +5,5 @@ const mongoURI = process.env.MONGODB_URI;
 
 mongoose.connect(mongoURI);
 
-const datosConexion = mongoose.connection
-datosConexion.once('open', () => { console.log('se conecto con exito') })
+const datosConexion = mongoose.connection;
+datosConexion.once('open', () => { console.log('se conecto con exito a la BD') })
