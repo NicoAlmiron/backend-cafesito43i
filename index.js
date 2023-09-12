2a8ed969ae8cbf5b3c9ef8dc01acdb444d4b1439
Nico Almiron <almiron.nicolas16@gmail.com>
2023-09-11 21:28:49 -0300

    configuramos correctamente la ruta

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ import productoRouter from './src/routes/productos.routes.js';
 import usuarioRouter from './src/routes/usuarios.routes.js';
 import './src/database/database.js';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 // 1- configuraciones iniciales
 
 const app = express();
+
+const _filename = fileURLToPath(
+    import.meta.url);
+const _dirname = path.dirname(_filename);
 //crear una variable con express
 app.set('port', process.env.PORT || 4000)
 app.listen(app.get('port'), () => {
@@ -23,8 +28,8 @@ app.use(express.urlencoded({ extended: true })); //ayuda entender datos tipo arr
 app.use(morgan('dev')); //herramienta de ayuda para el developer (nos da datos en la terminal)
 // console.log(path.join(__dirname, '/public'));
 // agregar un archivo estatico
-console.log(path.join('C:/Users/Nico/OneDrive/Escritorio/Proyectos/Nueva carpeta/backend-cafesito43i', '/public'));
-app.use(express.static(path.join('C:/Users/Nico/OneDrive/Escritorio/Proyectos/Nueva carpeta/backend-cafesito43i', '/public')));
+// console.log(path.join('C:/Users/Nico/OneDrive/Escritorio/Proyectos/Nueva carpeta/backend-cafesito43i', '/public'));
+app.use(express.static(path.join(_dirname, '/public')));
 // app.use(express.static(, '/public')));
 
 // 3- crear las rutas (siempre despues de los middlewares)
